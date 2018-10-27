4581f958c5f36153e5829708ca428ecc0fad9da2
tiankazma <tian_liu_1201@163.com>
2018-10-27 20:51:42 +0800

    start enumeration
    start enum practice

--- fibonacci.java
+++ fibonacci.java
@@ -1,22 +1,17 @@
 public class fibonacci{
 	//simple fibonacci
 	public void simple_fib(int i, int j, int t){
-		sys("Simple Fibonacci:");
-		sys(String.valueOf(i));
-		sys(String.valueOf(j));
+		test.sys("Simple Fibonacci:");
+		test.sys(String.valueOf(i));
+		test.sys(String.valueOf(j));
 
 		for(int x=0; x<t; x++){
 			int z = 0;
 			z = i+j;
 			i = j;
 			j = z;
-			
-			sys(String.valueOf(z));
-		}
-	}
 
-	//实例化 system.out.println():
-	public void sys(String ans){
-		System.out.println(ans);
-	}
+			test.sys(String.valueOf(z));
+		}
+	}	
 }
--- test.java
+++ test.java
@@ -1,6 +1,11 @@
 public class test{
+	//实例化 system.out.println():
+	public static void sys(String ans){
+		System.out.println(ans);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Testing\n");
+		sys("Testing\n");
 
 		//fibonacci
 		//i==num1, j==num2, time==fib times
