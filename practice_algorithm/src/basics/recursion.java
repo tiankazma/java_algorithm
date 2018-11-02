4b41ade387b3e10ff5faa05d182d28becb9a7976
tiankazma <tian_liu_1201@163.com>
2018-11-02 11:19:42 +0800

    update
    update

--- practice_algorithm/src/basics/recursion.java
+++ practice_algorithm/src/basics/recursion.java
@@ -1,16 +1,16 @@
 package basics;
 
-public class recursion{
+public class recursion {
 	// factorial
-	public static int factorial(int n){
-		if(n <= 1){
+	public static int factorial(int n) {
+		if (n <= 1) {
 			return 1;
-		}else{
+		} else {
 			return n * factorial(n - 1);
 		}
 	}
 
-	public static String get_result(int n){
+	public static String get_result(int n) {
 		return String.valueOf(factorial(n));
 	}
 }
